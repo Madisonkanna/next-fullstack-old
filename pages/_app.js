de9576e82d3470e5562d1ac687e0925be9d075d5
Madison <madisonkanna@gmail.com>
2022-06-01 20:24:53 -0700

    feat(_app): add sessionprovider

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,17 @@ import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/provider";
 import Layout from "../components/Layout";
 import theme from "../styles/theme";
+import { SessionProvider } from "next-auth/react";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <SessionProvider session={pageProps.session}>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </SessionProvider>
   );
 };
 
