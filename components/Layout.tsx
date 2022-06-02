52345adbe8a7b1380f97de60e677bb7458c4cc83
Madison <madisonkanna@gmail.com>
2022-06-01 21:00:36 -0700

    chore(layout): do not display sidebar on login

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,21 +1,33 @@
 import { Box } from "@chakra-ui/react";
 import NavigationBar from "./NavigationBar";
+import { Router, useRouter } from "next/router";
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+  const onLogin = router.route === "/login";
   return (
     <Box>
-      <Box marginLeft={{ sm: "0", md: "0", lg: "250px", xl: "250xp" }}>
+      <Box
+        marginLeft={!onLogin && { sm: "0", md: "0", lg: "250px", xl: "250xp" }}
+      >
         {children}
       </Box>
-      <Box
-        bottom={{ sm: "0", md: "0" }}
-        width={{ lg: "250px", xl: "250px" }}
-        top={{ lg: "0", xl: "0" }}
-        left={{ lg: "0", xl: "0" }}
-        position={{ sm: "fixed", md: "fixed", lg: "absolute", xl: "absolute" }}
-      >
-        <NavigationBar />
-      </Box>
+      {!onLogin && (
+        <Box
+          bottom={{ sm: "0", md: "0" }}
+          width={{ lg: "250px", xl: "250px" }}
+          top={{ lg: "0", xl: "0" }}
+          left={{ lg: "0", xl: "0" }}
+          position={{
+            sm: "fixed",
+            md: "fixed",
+            lg: "absolute",
+            xl: "absolute",
+          }}
+        >
+          <NavigationBar />
+        </Box>
+      )}
     </Box>
   );
 };
