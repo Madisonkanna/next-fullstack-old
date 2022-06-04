f2323c663bc1d2996edaf5fce0a54a7f8e37fe42
Madison <madisonkanna@gmail.com>
2022-06-04 13:17:27 -0700

    wip: add auth

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,41 @@
 import { useRouter } from "next/router";
 import { FC } from "react";
+import { Box } from "@chakra-ui/react";
 import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+
+type CallbackFunctionOrSomething = (pathname: string) => boolean;
 
 const Login: FC = () => {
-  const { data: session, status } = useSession();
-  return <div>Sign in page</div>;
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const isActive = (pathname: string): boolean => {
+    return router.pathname === pathname;
+  };
+  return (
+    <Box>
+      <h1>Sign in page</h1>
+      <Link href="/">
+        <a className="bold" data-active={isActive("/")}>
+          Feed
+        </a>
+      </Link>
+      {!session && (
+        <Link href="/api/auth/signin">
+          <a data-active={isActive("/signup")}>Log in</a>
+        </Link>
+      )}
+      {session && (
+        <div>
+          A session!
+          <button onClick={() => signOut()}>
+            <a>Log out</a>
+          </button>
+        </div>
+      )}
+    </Box>
+  );
 };
 
 export default Login;
